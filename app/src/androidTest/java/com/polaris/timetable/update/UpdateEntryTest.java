852a6b27ec5b7947ac86e15faa274f9c817b1b3f
sdhou <3344063785@qq.com>
2026-09-06 19:14:09 +0800

    test(ci): 更新入口测试滚动到目标再点击，失败上传 Espresso 报告工件定位仅 CI 复现的断言 (1.27.9)

--- app/src/androidTest/java/com/polaris/timetable/update/UpdateEntryTest.java
+++ app/src/androidTest/java/com/polaris/timetable/update/UpdateEntryTest.java
@@ -28,6 +28,7 @@ import java.nio.charset.StandardCharsets;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -86,7 +87,7 @@ public class UpdateEntryTest {
     public void checkUpdateRowsAreReachableInMorePage() {
         scenario = ActivityScenario.launch(MainActivity.class);
         onView(withNavLabel("我的")).perform(click());
-        onView(withText(R.string.my_card_more)).perform(click());
+        onView(withText(R.string.my_card_more)).perform(scrollTo(), click());
         onView(withText(R.string.settings_row_check_update)).check(matches(isDisplayed()));
         onView(withText(R.string.settings_row_auto_check_update)).check(matches(isDisplayed()));
         onView(withText(R.string.settings_row_contact)).check(matches(isDisplayed()));
@@ -98,10 +99,11 @@ public class UpdateEntryTest {
     public void tappingCheckRowRepeatedlyDoesNotCrash() {
         scenario = ActivityScenario.launch(MainActivity.class);
         onView(withNavLabel("我的")).perform(click());
-        onView(withText(R.string.my_card_more)).perform(click());
-        onView(withText(R.string.settings_row_check_update)).perform(click());
+        onView(withText(R.string.my_card_more)).perform(scrollTo(), click());
+        onView(withText(R.string.settings_row_check_update)).perform(scrollTo(), click());
         // 防重复：立即再点一次不应崩溃（协调器会忽略进行中的重复检查）。
-        onView(withText(R.string.settings_row_check_update)).perform(click());
+        onView(withText(R.string.settings_row_check_update)).perform(scrollTo(), click());
+        onView(withText(R.string.settings_row_version)).perform(scrollTo());
         onView(withText(R.string.settings_row_version)).check(matches(isDisplayed()));
     }
 
